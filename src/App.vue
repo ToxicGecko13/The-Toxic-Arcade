672f56f4014ad0dc54afc894479d350529010a34
Carsten Ruhl <ToxicGecko13@Gmail.com>
2019-05-28 23:27:52 -0700

    Fourth Commit - nav menu restyling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-
-    <nav>
-      <div id="usernameBox"><Username Username="XxToxicGeckoxX"/></div>
+    <div v-if="menuSize = true" class="toggleNvBuContainer">
+      <button class="toggleNvBu" id="on" v-on:click="openNav">&#8801;</button>  
+    </div>
+    <div v-else class="toggleNvBuContainer">
+      <button class="toggleNvBu" id="off" v-on:click="closeNav">&#8801;</button>
+    </div>
+    <!-- <button id="toggleNavMenu" v-on:click="toggleMenu">&#8801;</button> -->
+    <nav id="navMenu">
+      <!-- <div id="usernameBox"><Username Username="XxToxicGeckoxX"/></div> -->
       <router-link class="navItems" id="homeButton"  to="/">Home</router-link>
-      <router-link class="navItems" to="/HighScores">High Scores</router-link>
+      <!-- <span id="underline"></span> -->
       <router-link class="navItems" to="/Game">Game</router-link>
     </nav>
 
@@ -20,6 +26,27 @@ export default {
   name: 'home',
   components: {
     Username
+  },
+  methods: {
+    // Set the width of the side navigation to 250px
+    openNav: function () {
+      document.getElementById("navMenu").style.width = "250px";
+      document.getElementById("app").style.marginLeft = "250px";
+    },
+
+    // Set the width of the side navigation to 0
+    closeNav: function () {
+      document.getElementById("navMenu").style.width = "0";
+      document.getElementById("app").style.marginLeft = "0";
+    },
+
+    menuSize: function () {
+      let navMenuWidth = document.getElementById("navMenu").style.width
+
+      if (navMenuWidth = 0) {
+        return true
+      }
+    }
   }
 }
 
@@ -35,49 +62,69 @@ export default {
   margin: 0;
 }
 
+/* The nav show button */
+.toggleNvBuContainer {
+  border: 0;
+  background-color: rgba(29, 29, 29, 0.267);
+  float: left;
+  padding: 5px 15px;
+  border-radius: 5px;
+  margin-left: 15px;
+}
+
+.toggleNvBu {
+  color: rgba(68, 68, 68, 0.5);
+  font-size: 40px;
+  border: 0;
+  background: none;
+}
+
+/* #underline {
+  width: 100%;
+  height: 5px;
+  background-color: gray;
+} */
+
+/* The side navigation menu */
 nav {
-  margin: 35px;
-  border-radius: 100px;
-  background-color: rgb(35, 35, 35);
-  font-size: 18px;
-  overflow: hidden;
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: rgb(17, 17, 17);
+  overflow-x: hidden;
+  padding-top: 60px;
+  /* transition: 0.5s; 0.5 second transition effect to slide in the sidenav */
 }
 
+/* The navigation menu links */
 .navItems {
-  padding: 19px;
-  margin-left: 20px;
-  font-weight: bold;
-  color: #919191;
+  padding: 10px 8px 8px 32px;
+  border: 0;
+  margin-bottom: 3px;
   text-decoration: none;
-  text-align: center;
+  font-size: 25px;
+  color: rgb(129, 129, 129);
   display: block;
-  float: right;
+  transition: 0.3s;
 }
 
-.navItems:hover {
+/* When the cursor is over a navigation link, change it's color */
+nav .navItems:hover {
+  color: rgb(241, 241, 241);
   background-color: rgb(121, 111, 111);
 }
 
-.navItems:active {
+/* When nav items are active change their style */
+nav .navItems:active {
   background-color: rgb(90, 90, 90);
   color: #15be15;
 }
 
-#homeButton {
-  margin-left: 30px;
-  float: left;
-}
-
+/* When the page is where a button is linked to */
 nav a.router-link-exact-active {
   color: #15be15;
-}
-
-#usernameBox {
-  margin-left: 15px;
-  margin-right: 10px;
-  font-weight: bold;
-  text-align: center;
-  display: block;
-  float: left;
 }
 </style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="gamePage">
-    <h1>Scroll down intel you can see all of the game board.</h1>
+    <h1 v-on:click="startGame" id="click">Scroll down intel you can see all of the game board.</h1>
     <canvas id="gameBoard">
-      <div id="battleship">X</div>
+      <!-- <div id="battleship">X</div> -->
     </canvas>
   </div>
 </template>
 
 <script>
 export default {
+  // mounted: function () {
+  // },
   methods: {
-    // test: function() {
-    //   let canvas = document.querySelector('canvas');
-    //   canvas.width = window.innerWidth;
-    //   canvas.height = window.innerHeight;
-    // },
-    startGame: function() {
+    startGame: function () {
       this.setupGame()
+      let gameBoard = document.getElementById('gameBoard')
+      let c = gameBoard.getContext('2d')
+      c.fillRect(80, 110, 2000, 2000)
     },
-    setupGame: function() {
+    setupGame: function () {
 
     },
-    shipMove: function() {
+    shipMove: function () {
 
     }
   }
@@ -30,14 +30,19 @@ export default {
 
 <style scoped>
   #gameBoard {
-    background-color: black;
+    /* background-color: black; */
+    background-color: rgb(128, 93, 93);
     padding: 18% 34%;
     border: 10px solid;
-    border-color: rgb(54, 54, 54);
+    border-color: rgb(37, 37, 37);
     margin: 15px;
   }
   #battleship {
     padding: 5px 5px;
     background-color: rgb(12, 189, 12);
+  }
+  #click:hover {
+    cursor: pointer;
+    color: rgb(59, 59, 59);
   }
 </style>
