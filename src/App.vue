ec9a13a0c8443991fe1e555ca74d324ecbd354a9
Carsten Ruhl <ToxicGecko13@Gmail.com>
2019-06-07 17:22:01 -0700

    Seventh Commit - changes to pages and new favicon

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,13 @@
     <nav id="navMenu">
       <button v-on:click="closeNav" id="closeNavBu">&times;</button>
       <br>
-      <!-- <div id="usernameBox"><Username Username="XxToxicGeckoxX"/></div> -->
       <router-link class="navItems" id="homeButton"  to="/">Home</router-link>
-      <router-link class="navItems" to="/Game">Game</router-link>
     </nav>
+
+    <h1>Scroll down until you can see all of the game board.</h1>
+    <button id="startGame" v-on:click="this.setUpGame" v-on:keyup.space="drawBullet">Start Game</button>
+    <canvas id="gameBoard">
+    </canvas>
 
     <router-view/>
 
@@ -15,13 +18,8 @@
 </template>
 
 <script>
-import Username from '@/components/Username.vue'
-
 export default {
   name: 'home',
-  components: {
-    Username
-  },
   methods: {
     // Function to open the nave menu from the left to the right
     openNav: function () {
@@ -31,10 +29,114 @@ export default {
     // Function to close the nave menu from the right to the left
     closeNav: function () {
       document.getElementById('navMenu').style.width = '0'
+    },
+
+    // Set up the game on the canvas (the Game Board)
+    setUpGame: function () {
+      let gameBoard = document.querySelector('canvas')
+      let c = gameBoard.getContext('2d')
+
+      // Example of how to draw a rectangle and color it
+      // c.fillStyle = 'rgba(0-255, 0-255, 0-255, 1-0.1)'
+      // c.fillRect(x, y, width, height)
+
+      // Draw battleship
+      c.fillStyle = 'rgba(255, 0, 148)'
+      // let battleship = c.fillRect(146, 136, 8, 8)
+      c.fillRect(146, 136, 8, 8)
+
+      this.drawAliens()
+      this.drawStars()
+    },
+
+    // Draw the background stars on the Game Board
+    drawStars: function () {
+      let gameBoard = document.querySelector('canvas')
+      let c = gameBoard.getContext('2d')
+
+      let hei = Math.floor(Math.random() * 146) + 2
+      let wid = Math.floor(Math.random() * 296) + 2
+
+      // Draw a star at a random location
+      c.beginPath()
+      c.strokeStyle = 'rgba(255, 255, 255, 0.4)'
+      c.arc(wid, hei, 0.4, 0, Math.PI * 2, false)
+      c.stroke()
+    },
+
+    // Draw the enemy aliens on the Game Board
+    drawAliens: function () {
+      let gameBoard = document.querySelector('canvas')
+      let c = gameBoard.getContext('2d')
+
+      // Draw first line of aliens
+      c.fillStyle = 'rgba(255, 0, 0)'
+      c.fillRect(19, 10, 10, 10)
+      c.fillRect(37, 10, 10, 10)
+      c.fillRect(55, 10, 10, 10)
+      c.fillRect(73, 10, 10, 10)
+      c.fillRect(91, 10, 10, 10)
+      c.fillRect(109, 10, 10, 10)
+      c.fillRect(127, 10, 10, 10)
+      c.fillRect(145, 10, 10, 10)
+      c.fillRect(163, 10, 10, 10)
+      c.fillRect(181, 10, 10, 10)
+      c.fillRect(199, 10, 10, 10)
+      c.fillRect(217, 10, 10, 10)
+      c.fillRect(235, 10, 10, 10)
+      c.fillRect(253, 10, 10, 10)
+      c.fillRect(271, 10, 10, 10)
+
+      // Draw Second line of aliens
+      c.fillStyle = 'rgba(255, 70, 0)'
+      c.fillRect(20, 29, 8, 8)
+      c.fillRect(38, 29, 8, 8)
+      c.fillRect(56, 29, 8, 8)
+      c.fillRect(74, 29, 8, 8)
+      c.fillRect(92, 29, 8, 8)
+      c.fillRect(110, 29, 8, 8)
+      c.fillRect(128, 29, 8, 8)
+      c.fillRect(146, 29, 8, 8)
+      c.fillRect(164, 29, 8, 8)
+      c.fillRect(182, 29, 8, 8)
+      c.fillRect(200, 29, 8, 8)
+      c.fillRect(218, 29, 8, 8)
+      c.fillRect(236, 29, 8, 8)
+      c.fillRect(254, 29, 8, 8)
+      c.fillRect(272, 29, 8, 8)
+
+      // Draw third line of aliens
+      c.fillStyle = 'rgba(255, 70, 0)'
+      c.fillRect(20, 46, 8, 8)
+      c.fillRect(38, 46, 8, 8)
+      c.fillRect(56, 46, 8, 8)
+      c.fillRect(74, 46, 8, 8)
+      c.fillRect(92, 46, 8, 8)
+      c.fillRect(110, 46, 8, 8)
+      c.fillRect(128, 46, 8, 8)
+      c.fillRect(146, 46, 8, 8)
+      c.fillRect(164, 46, 8, 8)
+      c.fillRect(182, 46, 8, 8)
+      c.fillRect(200, 46, 8, 8)
+      c.fillRect(218, 46, 8, 8)
+      c.fillRect(236, 46, 8, 8)
+      c.fillRect(254, 46, 8, 8)
+      c.fillRect(272, 46, 8, 8)
+    },
+
+    // Draw a bullet on the Game Board
+    drawBullet: function () {
+      let gameBoard = document.querySelector('canvas')
+      let c = gameBoard.getContext('2d')
+
+      // Draw a bullet
+      c.fillStyle = 'rgba(255, 255, 0)'
+      c.fillRect(149, 130, 2, 2)
+      c.fillRect(149, 122, 2, 2)
+      c.fillRect(149, 114, 2, 2)
     }
   }
 }
-
 </script>
 
 <style scoped>
@@ -120,5 +222,41 @@ nav .navItems:hover {
 
 nav a.router-link-exact-active {
   color: #15be15;
+}
+
+#gameBoard {
+    background-color: rgb(0, 0, 0);
+    height: 700px;
+    width: 1350px;
+    border: 10px solid;
+    border-color: rgb(59, 59, 59);
+    margin: 5px;
+    margin-top: 45px;
+    margin-left: -75px;
+}
+
+#battleship {
+    padding: 5px 5px;
+    background-color: rgb(12, 189, 12);
+}
+
+#startGame {
+    float: right;
+    /* margin-top: -20px; */
+    margin-top: 30px;
+    margin-bottom: 40px;
+    margin-right: 10px;
+    border: 0;
+    padding: 8px;
+    padding-right: 14px;
+    padding-left: 14px;
+    border-radius: 5px;
+    background-color: rgb(58, 58, 58);
+    color: whitesmoke;
+}
+
+#startGame:hover {
+    cursor: pointer;
+    background-color: rgb(70, 70, 70);
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -11,15 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
-    },
-    {
-      path: '/Game',
-      name: 'Game',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "Game" */ './views/Game.vue')
+      // component: Home
     },
     {
       path: '/LogIn',
